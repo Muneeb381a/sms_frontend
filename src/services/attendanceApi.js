bb3adbf88d3227f4b420d39d47b139eef98bee71
Muneeb381a <muneebafzal381a@gmail.com>
2025-05-01 11:42:01 +0500

    Fix formatting in attendanceApi.js: remove unnecessary blank line

--- src/services/attendanceApi.js
+++ src/services/attendanceApi.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const API_BASE = 'http://localhost:3500/api/v1/attendance';
 
+
 export const fetchAttendance = async (page = 1, limit = 10) => {
   const response = await axios.get(`${API_BASE}?page=${page}&limit=${limit}`);
   return {
